2e43c565c27e424a6025df3bbbbbed262298e541
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-17 18:13:18 +0530

    API Versioned

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  resources :tasks, only: [:create, :index, :show, :update, :destroy]
+  # resources :tasks, only: [:create, :index, :show, :update, :destroy]
+  concern :api_base do
+    resources :tasks
+  end
+
+  namespace :v1 do
+    concerns :api_base
+  end
 end
