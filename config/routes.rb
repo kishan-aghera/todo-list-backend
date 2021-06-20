3edc5e4e68fd90a472c541077d8d8e0eaf7f1ed7
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-20 17:46:43 +0530

    Added routes for Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # resources :tasks, only: [:create, :index, :show, :update, :destroy]
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/logged_in', to: 'sessions#is_logged_in?'
+  
   concern :api_base do
     resources :users, only: [:create, :show, :index] do
       resources :tasks
