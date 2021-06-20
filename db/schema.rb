c5483b8d15ac6eb94b11e3d04f951b024491367f
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-20 17:33:56 +0530

    Added User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_112740) do
+ActiveRecord::Schema.define(version: 2021_06_20_120004) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
