6e45301412a37e35308db0dc08b1ba419b83090f
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-20 17:45:16 +0530

    Added Sessions Controller

---
+++ app/controllers/v1/sessions_controller.rb
@@ -0,0 +1,48 @@
+module V1
+  class SessionsController < ApplicationController
+    def create
+      @user = User.find_by(email: session_params[:email])
+    
+      if @user && @user.authenticate(session_params[:password]) # .authenticate will work with has_secure_password found in User model
+        login!
+        render json: {
+          logged_in: true,
+          user: @user
+        }
+      else
+        render json: { 
+          status: 401,
+          errors: ['No such User', 'Verify Credentials and Try Again or Signup']
+        }
+      end
+    end
+    
+    def is_logged_in?
+      if logged_in? && current_user
+        render json: {
+          logged_in: true,
+          user: current_user
+        }
+      else
+        render json: {
+          logged_in: false,
+          message: 'No such user'
+        }
+      end
+    end
+    
+    def destroy
+      logout!
+      render json: {
+        status: 200,
+        logged_out: true
+      }
+    end
+    
+    private
+    
+    def session_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+  end
+end
