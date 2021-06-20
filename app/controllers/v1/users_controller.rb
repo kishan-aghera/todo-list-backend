cfb3929ade1f95396bdcb8c0096b9480f60244cb
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-20 17:45:04 +0530

    Formatted Users and Application Controller

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,31 +1,31 @@
 module V1
   class UsersController < ApplicationController
     def index
-        @users = User.all
-        if @users
-          render json: {
-            users: @users
-          }
-        else
-          render json: {
-            status: 500,
-            errors: ['no users found']
-          }
-        end
+      @users = User.all
+      if @users
+        render json: {
+          users: @users
+        }
+      else
+        render json: {
+          status: 500,
+          errors: ['no users found']
+        }
+      end
     end
   
     def show
-        @user = User.find(params[:id])
-       if @user
-          render json: {
-            user: @user
-          }
-        else
-          render json: {
-            status: 500,
-            errors: ['user not found']
-          }
-        end
+      @user = User.find(params[:id])
+      if @user
+        render json: {
+          user: @user
+        }
+      else
+        render json: {
+          status: 500,
+          errors: ['user not found']
+        }
+      end
     end
       
     def create
