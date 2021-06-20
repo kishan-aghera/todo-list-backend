d5deafe18edaf44519b965766ff1c08f1f2ffa25
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-20 17:37:54 +0530

    Configured routes for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # resources :tasks, only: [:create, :index, :show, :update, :destroy]
   concern :api_base do
-    resources :tasks
+    resources :users, only: [:create, :show, :index] do
+      resources :tasks
+    end
   end
 
   namespace :v1 do
