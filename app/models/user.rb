457e69d2e07f914de37c347f919cf3b789d92f09
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-23 17:36:30 +0530

    Established relations between tasks and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
-  
-  validates :username, presence: true, uniqueness: true, length: { minimum: 6 }
+  has_many :tasks
+
   validates :email, presence: true, uniqueness: true
   
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
