b22619b4e4df9a4c210221cc19dbdae535309250
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-06-23 14:00:46 +0530

    Removed API versioning as it was not needed for now

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
   include CurrentUserConcern
-  
+
   def create
     user = User
-              .find_by(email: params["user"]["email"])
-              .try(:authenticate, params["user"]["password"])
+            .find_by(email: params["user"]["email"])
+            .try(:authenticate, params["user"]["password"])
 
     if user
       session[:user_id] = user.id
@@ -14,9 +14,7 @@ class SessionsController < ApplicationController
         user: user
       }
     else
-      render json: {
-        status: 401
-      }
+      render json: { status: 401 }
     end
   end
 
@@ -35,9 +33,6 @@ class SessionsController < ApplicationController
 
   def logout
     reset_session
-    render json: {
-      status: 200,
-      logged_out: true
-    }
+    render json: { status: 200, logged_out: true }
   end
 end
--- app/serializers/v1/tasks_serializer.rb
+++
@@ -1,5 +0,0 @@
-module V1
-  class TasksSerializer < ActiveModel::Serializer
-    attributes :id, :name
-  end
-end
