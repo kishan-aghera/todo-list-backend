3dfa458d60cc4279d41cbf10eb87818f8ea7526f
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-07-06 15:26:55 +0530

    Added url of frontend app

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000", "https://todo-list-rails-api.herokuapp.com"
 
     resource '*',
       headers: :any,
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env === 'production' 
-  Rails.application.config.session_store :cookie_store, key: '_todo-list-backend', domain: 'your-frontend-domain'
+  Rails.application.config.session_store :cookie_store, key: '_todo-list-backend', domain: 'https://todo-list-rails-api.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: '_todo-list-backend' 
 end
