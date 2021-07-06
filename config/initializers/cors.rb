8e78b67281bd9e6a79f73bf6f2b4eee8ca56912c
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-07-06 15:31:45 +0530

    Fixed typo in session_store.rb and cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://kishan-aghera.github.io/todo-list-frontend/"
+    origins "http://localhost:3000", "https://kishan-aghera.github.io/todo-list-frontend"
 
     resource '*',
       headers: :any,
