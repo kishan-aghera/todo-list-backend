8cb1f78c766a38ac57f7ac6430bacc953af64a18
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-07-09 11:12:19 +0530

    Update key and added local ip in cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000", "https://kishan-aghera.github.io"
+    origins "http://localhost:3000", "https://kishan-aghera.github.io", "127.0.0.1:3000"
 
     resource '*',
       headers: :any,
